039b770d02c98c6b2c94909d49d00e231932b132
Marek Serafin <m.serafin@kontakt.io>
2016-06-11 01:29:42 +0200

    Update sample workshop code to 1.2.0-beta1 SDK version + Added Beaconvalley Hackathon sample code

---
+++ Part 5 - Beaconvalley Hackathon/SDK Workshop/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SDK Workshop
+//
+//  Created by Marek Serafin on 15/02/16.
+//  Copyright © 2016 Marek Serafin. All rights reserved.
+//
+
+import UIKit
+import KontaktSDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Set your API Key
+        Kontakt.setAPIKey("<#Api Key#>")
+                
+        return true
+    }
+}
+
---
+++ Part 5 - Beaconvalley Hackathon/SDK Workshop/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  SDK Workshop
+//
+//  Created by Marek Serafin on 15/02/16.
+//  Copyright © 2016 Marek Serafin. All rights reserved.
+//
+
+import UIKit
+import KontaktSDK
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    var devicesManager: KTKDevicesManager!
+    var connection: KTKDeviceConnection?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initiate Devices Manager
+        devicesManager = KTKDevicesManager(delegate: self)
+        
+        // Start Discovery
+        devicesManager.startDevicesDiscoveryWithInterval(2.0)
+    }
+}
+
+extension ViewController: KTKDevicesManagerDelegate {
+    
+    func devicesManagerDidFailToStartDiscovery(manager: KTKDevicesManager, withError error: NSError?) {
+        
+    }
+    
+    func devicesManager(manager: KTKDevicesManager, didDiscoverDevices devices: [KTKNearbyDevice]?) {
+        
+        print("Devices Manager found \(devices?.count) kontakt devices")
+        
+        if let device = devices?.filter({ $0.uniqueID == "<#unique id#>" }).first {
+            manager.stopDevicesDiscovery()
+            
+            let configuration = KTKDeviceConfiguration()
+            configuration.motionDetectionMode = .Alarm
+            
+            connection = KTKDeviceConnection(nearbyDevice: device)
+            
+            if let connection = connection {
+                // Write Configuration
+                connection.writeConfiguration(configuration) { synchronized, configuration, error in
+                    if let _ = error {
+                        print("Error while configuring")
+                    }
+                    else {
+                        print("Configuration applied")
+                    }
+                }
+            }
+        }
+    }
+}
+
